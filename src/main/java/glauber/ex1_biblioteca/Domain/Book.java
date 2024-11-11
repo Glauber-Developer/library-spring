f012b3efc1f591c187e1fb526c38c067af5fdd60
Glauber-Araujo <dev02@vrupi.com>
2024-11-11 10:53:24 -0300

    chore: initial files

---
+++ src/main/java/glauber/ex1_biblioteca/Domain/Book.java
@@ -0,0 +1,47 @@
+package glauber.ex1_biblioteca.Domain;
+
+public class Book {
+    private int id;
+    private String titulo;
+    private String autor;
+    private String ano;
+
+    public Book(int id, String titulo, String autor, String ano) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+}
