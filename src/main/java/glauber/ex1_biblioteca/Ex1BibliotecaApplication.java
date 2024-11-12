24c376a9b8f3cfeffd0fd61a97d8345e1998c5a2
Glauber-Araujo <dev02@vrupi.com>
2024-11-12 15:39:33 -0300

    #7

--- src/main/java/glauber/ex1_biblioteca/Ex1BibliotecaApplication.java
+++ src/main/java/glauber/ex1_biblioteca/Ex1BibliotecaApplication.java
@@ -1,12 +1,17 @@
 package glauber.ex1_biblioteca;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class Ex1BibliotecaApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(Ex1BibliotecaApplication.class, args);
 	}
 
